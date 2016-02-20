c7cd0be1fcddf5b7135b25a66ffeeac536eaa076
Yilson <ing.jimenezr13@gmail.com>
2016-02-20 18:52:35 -0400

    Estableciendo Relaciones de las Tablas

---
+++ Soft_P3/Entidades/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soft_P3.Entidades
+{
+    class Categoria
+    {
+        public int Id { get; private set; }
+        public string Descripcion { get; private set; }
+    }
+}
---
+++ Soft_P3/Entidades/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soft_P3.Entidades
+{
+    class Cliente
+    {
+        private int Id;
+        private string NombCliente;
+      ,[ApelliCliente]
+      ,[Telefono]
+      ,[Celular]
+      ,[Email]
+      ,[Cedula]
+      ,[Direccion]
+      ,[Nacionalidad]
+      ,[RNC]
+      ,[Fecha]
+    }
+}
