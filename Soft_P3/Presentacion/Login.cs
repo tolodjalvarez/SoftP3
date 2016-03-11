bf3752460faa64ae3016494ae5eed973d119d4e6
AlphaDogg <xjdalt@hotmail.com>
2016-03-10 23:24:00 -0400

    Diseño Formulario Login
    Falta el Icono.
    Faltan Efectos de textboxes.

---
+++ Soft_P3/Presentacion/Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soft_P3.Presentacion
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private static Login _instancia;
+
+        public static Login GetInstance()
+        {
+            if (_instancia == null)
+                _instancia = new Login();
+            return _instancia;
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            FrmMenu fm = new FrmMenu();
+            fm.Show();
+        }
+
+        
+    }
+}
--- Soft_P3/Program.cs
+++ Soft_P3/Program.cs
@@ -17,7 +17,7 @@ namespace Soft_P3
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run( FrmMenu.GetInstance());
+            Application.Run( Login.GetInstance());
         }
     }
 }
